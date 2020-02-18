29f38dc10461aed9a4d8c82ca5dbadfa27ff1cf3
lpadron18 <escalerapadronl@berea.edu>
2020-02-18 11:25:29 -0500

    Created function and main function.

--- top100.py
+++ top100.py
@@ -8,28 +8,29 @@ soup = BeautifulSoup(url.content, 'lxml')
 
 chart_wrapper = soup.find('div', class_="chart-wrapper")
 
+def collect_data():
+    count = 1
+    song_details = {
+        'title': [],
+        'artist': []
+    }
+    for child in chart_wrapper.children:
+        song_box = chart_wrapper.find_all('div', id="chart-position-" + str(count))
+        count += 1
+        for song in song_box:
+            song_info = song.find_all('p', class_='title-artist')
+            for song in song_info:
+                song_title = song.find("cite", class_='title')
+                song_artist = song.find('em', class_='artist')
+                song_details['title'].append(song_title.text.lower())
+                song_details["artist"].append(song_artist.text.lower())
 
-count = 1
-song_details = {
-    'title': [],
-    'artist': []
-}
-for child in chart_wrapper.children:
-    song_box = chart_wrapper.find_all('div', id="chart-position-" + str(count))
-    count += 1
-    for song in song_box:
-        song_info = song.find_all('p', class_='title-artist')
-        for song in song_info:
-            song_title = song.find("cite", class_='title')
-            song_artist = song.find('em', class_='artist')
-            song_details['title'].append(song_title.text.lower())
-            song_details["artist"].append(song_artist.text.lower())
-print(song_details)
-
-df = pd.DataFrame(song_details)
-df = df.drop_duplicates()
-print(df)
-df.to_csv("songinfo.csv")
+    df = pd.DataFrame(song_details)
+    df = df.drop_duplicates()
+    df.to_csv("songinfo.csv")
 
 
+def main():
+    collect_data()
 
+main()
